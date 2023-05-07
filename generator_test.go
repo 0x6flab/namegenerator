2346f9f6fbfac3c48dc20c8de7140962b0fa0e59
rodneyosodo <blackd0t@protonmail.com>
2023-05-07 18:08:16 +0300

    Fix Tests
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>

--- generator_test.go
+++ generator_test.go
@@ -22,69 +22,7 @@ func TestNameGenerator_Generate(t *testing.T) {
 		t.Errorf("Expected a name, got an empty string")
 	}
 
-	if !strings.Contains(name, "-") {
-		t.Errorf("Expected a name with a family name separator, got %s", name)
-	}
-
-	if !contains(namegenerator.GeneralNames, strings.Split(name, "-")[0]) {
-		t.Errorf("Generated name '%s' does not contain a general name", name)
-	}
-
-	if !contains(namegenerator.FamilyNames, strings.Split(name, "-")[1]) {
-		t.Errorf("Generated name '%s' does not contain a family name", name)
-	}
-
-	generator = namegenerator.NewNameGenerator("male")
-	name = generator.Generate()
-
-	if len(name) == 0 {
-		t.Errorf("Expected a male name, got an empty string")
-	}
-
-	if !strings.Contains(name, "-") {
-		t.Errorf("Expected a male name with a family name separator, got %s", name)
-	}
-
-	if !contains(namegenerator.MaleNames, strings.Split(name, "-")[0]) {
-		t.Errorf("Generated name '%s' does not contain a male name", name)
-	}
-
-	if !contains(namegenerator.FamilyNames, strings.Split(name, "-")[1]) {
-		t.Errorf("Generated name '%s' does not contain a family name", name)
-	}
-
-	generator = namegenerator.NewNameGenerator("female")
-	name = generator.Generate()
-
-	if len(name) == 0 {
-		t.Errorf("Expected a female name, got an empty string")
-	}
-
-	if !strings.Contains(name, "-") {
-		t.Errorf("Expected a female name with a family name separator, got %s", name)
-	}
-
-	if !contains(namegenerator.FemaleNames, strings.Split(name, "-")[0]) {
-		t.Errorf("Generated name '%s' does not contain a female name", name)
-	}
-
-	if !contains(namegenerator.FamilyNames, strings.Split(name, "-")[1]) {
-		t.Errorf("Generated name '%s' does not contain a family name", name)
-	}
-}
-
-func TestNameGenerator_GenerateNames(t *testing.T) {
-	generator := namegenerator.NewNameGenerator("")
-	names := generator.GenerateNames(5)
-
-	if len(names) != 5 {
-		t.Errorf("Expected 5 names, got %d", len(names))
-	}
-
-	for _, name := range names {
-		if len(name) == 0 {
-			t.Errorf("Expected a name, got an empty string")
-		}
+	if strings.Count(name, "-") == 1 {
 
 		if !strings.Contains(name, "-") {
 			t.Errorf("Expected a name with a family name separator, got %s", name)
@@ -98,15 +36,11 @@ func TestNameGenerator_GenerateNames(t *testing.T) {
 			t.Errorf("Generated name '%s' does not contain a family name", name)
 		}
 	}
-
 	generator = namegenerator.NewNameGenerator("male")
-	names = generator.GenerateNames(5)
+	name = generator.Generate()
 
-	if len(names) != 5 {
-		t.Errorf("Expected 5 male names, got %d", len(names))
-	}
+	if strings.Count(name, "-") == 1 {
 
-	for _, name := range names {
 		if len(name) == 0 {
 			t.Errorf("Expected a male name, got an empty string")
 		}
@@ -125,6 +59,85 @@ func TestNameGenerator_GenerateNames(t *testing.T) {
 	}
 
 	generator = namegenerator.NewNameGenerator("female")
+	name = generator.Generate()
+
+	if strings.Count(name, "-") == 1 {
+
+		if len(name) == 0 {
+			t.Errorf("Expected a female name, got an empty string")
+		}
+
+		if !strings.Contains(name, "-") {
+			t.Errorf("Expected a female name with a family name separator, got %s", name)
+		}
+
+		if !contains(namegenerator.FemaleNames, strings.Split(name, "-")[0]) {
+			t.Errorf("Generated name '%s' does not contain a female name", name)
+		}
+
+		if !contains(namegenerator.FamilyNames, strings.Split(name, "-")[1]) {
+			t.Errorf("Generated name '%s' does not contain a family name", name)
+		}
+	}
+}
+
+func TestNameGenerator_GenerateNames(t *testing.T) {
+	generator := namegenerator.NewNameGenerator("")
+	names := generator.GenerateNames(5)
+
+	if len(names) != 5 {
+		t.Errorf("Expected 5 names, got %d", len(names))
+	}
+
+	for _, name := range names {
+		if len(name) == 0 {
+			t.Errorf("Expected a name, got an empty string")
+		}
+
+		if strings.Count(name, "-") == 1 {
+
+			if !strings.Contains(name, "-") {
+				t.Errorf("Expected a name with a family name separator, got %s", name)
+			}
+
+			if !contains(namegenerator.GeneralNames, strings.Split(name, "-")[0]) {
+				t.Errorf("Generated name '%s' does not contain a general name", name)
+			}
+
+			if !contains(namegenerator.FamilyNames, strings.Split(name, "-")[1]) {
+				t.Errorf("Generated name '%s' does not contain a family name", name)
+			}
+		}
+	}
+
+	generator = namegenerator.NewNameGenerator("male")
+	names = generator.GenerateNames(5)
+
+	if len(names) != 5 {
+		t.Errorf("Expected 5 male names, got %d", len(names))
+	}
+
+	for _, name := range names {
+		if len(name) == 0 {
+			t.Errorf("Expected a male name, got an empty string")
+		}
+
+		if strings.Count(name, "-") == 1 {
+			if !strings.Contains(name, "-") {
+				t.Errorf("Expected a male name with a family name separator, got %s", name)
+			}
+
+			if !contains(namegenerator.MaleNames, strings.Split(name, "-")[0]) {
+				t.Errorf("Generated name '%s' does not contain a male name", name)
+			}
+
+			if !contains(namegenerator.FamilyNames, strings.Split(name, "-")[1]) {
+				t.Errorf("Generated name '%s' does not contain a family name", name)
+			}
+		}
+	}
+
+	generator = namegenerator.NewNameGenerator("female")
 	names = generator.GenerateNames(5)
 
 	if len(names) != 5 {
@@ -136,16 +149,18 @@ func TestNameGenerator_GenerateNames(t *testing.T) {
 			t.Errorf("Expected a male name, got an empty string")
 		}
 
-		if !strings.Contains(name, "-") {
-			t.Errorf("Expected a male name with a family name separator, got %s", name)
-		}
+		if strings.Count(name, "-") == 1 {
+			if !strings.Contains(name, "-") {
+				t.Errorf("Expected a male name with a family name separator, got %s", name)
+			}
 
-		if !contains(namegenerator.FemaleNames, strings.Split(name, "-")[0]) {
-			t.Errorf("Generated name '%s' does not contain a female name", name)
-		}
+			if !contains(namegenerator.FemaleNames, strings.Split(name, "-")[0]) {
+				t.Errorf("Generated name '%s' does not contain a female name", name)
+			}
 
-		if !contains(namegenerator.FamilyNames, strings.Split(name, "-")[1]) {
-			t.Errorf("Generated name '%s' does not contain a family name", name)
+			if !contains(namegenerator.FamilyNames, strings.Split(name, "-")[1]) {
+				t.Errorf("Generated name '%s' does not contain a family name", name)
+			}
 		}
 	}
 }
