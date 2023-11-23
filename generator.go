c74eaf1cb89a639ccde611355f8588d19ee077ca
Rodney Osodo <28790446+rodneyosodo@users.noreply.github.com>
2023-11-23 21:00:30 +0300

    feat: Add prefix and suffix options
    The name generator can generate random names with different options that
    is, prefix and suffix.
    It includes test cases to verify the functionality of the name generator,
    including generating different types of names and checking if they meet the expected criteria.

--- generator.go
+++ generator.go
@@ -8,7 +8,6 @@ import (
 	"crypto/rand"
 	"math/big"
 	mrand "math/rand"
-	"sync"
 	"time"
 )
 
@@ -55,11 +54,33 @@ type NameGenerator interface {
 	// Output:
 	//  `John-Smith`
 	WithGender(gender Gender) NameGenerator
+
+	// WithPrefix generates a name with a prefix.
+	//
+	// Example:
+	//  generator := namegenerator.NewNameGenerator().WithPrefix("Mr. ")
+	//  name := generator.Generate()
+	//  fmt.Println(name)
+	// Output:
+	//  `Mr. John-Smith`
+	WithPrefix(prefix string) NameGenerator
+
+	// WithSuffix generates a name with a suffix.
+	//
+	// Example:
+	//  generator := namegenerator.NewNameGenerator().WithSuffix("@gmail.com")
+	//  name := generator.Generate()
+	//  fmt.Println(name)
+	// Output:
+	//  `John-Smith@gmail.com`
+	WithSuffix(suffix string) NameGenerator
 }
 
 // nameGenerator is a struct that implements NameGenerator.
 type nameGenerator struct {
 	gender Gender
+	prefix string
+	suffix string
 }
 
 // NewNameGenerator returns a new NameGenerator.
@@ -79,6 +100,14 @@ type nameGenerator struct {
 // Example to generate non-binary names:
 //
 //	generator := namegenerator.NewNameGenerator().WithGender(namegenerator.NonBinary)
+//
+// Example to generate names with a prefix:
+//
+//	generator := namegenerator.NewNameGenerator().WithPrefix("Mr. ")
+//
+// Example to generate names with a suffix:
+//
+//	generator := namegenerator.NewNameGenerator().WithSuffix("@gmail.com")
 func NewNameGenerator() NameGenerator {
 	return &nameGenerator{
 		gender: NonBinary,
@@ -91,42 +120,46 @@ func (namegen *nameGenerator) WithGender(gender Gender) NameGenerator {
 	return namegen
 }
 
+func (namegen *nameGenerator) WithPrefix(prefix string) NameGenerator {
+	namegen.prefix = prefix
+
+	return namegen
+}
+
+func (namegen *nameGenerator) WithSuffix(suffix string) NameGenerator {
+	namegen.suffix = suffix
+
+	return namegen
+}
+
 func (namegen *nameGenerator) Generate() string {
 	switch namegen.gender {
 	case Male:
 		grandom := namegen.generateRandomNumber(len(MaleNames))
 		frandom := namegen.generateRandomNumber(len(FamilyNames))
 
-		return MaleNames[grandom] + "-" + FamilyNames[frandom]
+		return namegen.prefix + MaleNames[grandom] + "-" + FamilyNames[frandom] + namegen.suffix
 	case Female:
 		grandom := namegen.generateRandomNumber(len(FemaleNames))
 		frandom := namegen.generateRandomNumber(len(FamilyNames))
 
-		return FemaleNames[grandom] + "-" + FamilyNames[frandom]
+		return namegen.prefix + FemaleNames[grandom] + "-" + FamilyNames[frandom] + namegen.suffix
 	case NonBinary:
+		fallthrough
+	// This condition never happens, but it's here to make the compiler happy.
+	default:
 		g1random := namegen.generateRandomNumber(len(GeneralNames))
 		g2random := namegen.generateRandomNumber(len(GeneralNames))
 
-		return GeneralNames[g1random] + "-" + GeneralNames[g2random]
-	// This condition never happens, but it's here to make the compiler happy.
-	default:
-		panic("invalid gender")
+		return namegen.prefix + GeneralNames[g1random] + "-" + GeneralNames[g2random] + namegen.suffix
 	}
 }
 
 func (namegen *nameGenerator) GenerateNames(count int) []string {
-	var waitGroup sync.WaitGroup
 	names := make([]string, count)
-
 	for i := 0; i < count; i++ {
-		waitGroup.Add(1)
-		go func(index int) {
-			defer waitGroup.Done()
-			names[index] = namegen.Generate()
-		}(i)
+		names[i] = namegen.Generate()
 	}
-
-	waitGroup.Wait()
 
 	return names
 }
--- generator_test.go
+++ generator_test.go
@@ -10,156 +10,171 @@ import (
 	"github.com/0x6flab/namegenerator"
 )
 
-func TestNewNameGenerator(t *testing.T) {
-	generator := namegenerator.NewNameGenerator()
-	if generator == nil {
-		t.Errorf("Expected NameGenerator, got nil")
+const (
+	prefix = "Mr. "
+	suffix = "@gmail.com"
+)
+
+func TestNameGenerator_Generate(t *testing.T) {
+	testcase := []struct {
+		description         string
+		generator           namegenerator.NameGenerator
+		expectedFirstNames  []string
+		expectedSecondNames []string
+		expectedPrefix      string
+		expectedSuffix      string
+	}{
+		{
+			description:         "generate default name",
+			generator:           namegenerator.NewNameGenerator(),
+			expectedFirstNames:  namegenerator.GeneralNames,
+			expectedSecondNames: namegenerator.GeneralNames,
+			expectedPrefix:      "",
+			expectedSuffix:      "",
+		},
+		{
+			description:         "generate male name",
+			generator:           namegenerator.NewNameGenerator().WithGender(namegenerator.Male),
+			expectedFirstNames:  namegenerator.MaleNames,
+			expectedSecondNames: namegenerator.FamilyNames,
+			expectedPrefix:      "",
+			expectedSuffix:      "",
+		},
+		{
+			description:         "generate female name",
+			generator:           namegenerator.NewNameGenerator().WithGender(namegenerator.Female),
+			expectedFirstNames:  namegenerator.FemaleNames,
+			expectedSecondNames: namegenerator.FamilyNames,
+			expectedPrefix:      "",
+			expectedSuffix:      "",
+		},
+		{
+			description:         "generate non-binary name",
+			generator:           namegenerator.NewNameGenerator().WithGender(namegenerator.NonBinary),
+			expectedFirstNames:  namegenerator.GeneralNames,
+			expectedSecondNames: namegenerator.GeneralNames,
+			expectedPrefix:      "",
+			expectedSuffix:      "",
+		},
+		{
+			description:         "generate default name with prefix",
+			generator:           namegenerator.NewNameGenerator().WithPrefix(prefix),
+			expectedFirstNames:  namegenerator.GeneralNames,
+			expectedSecondNames: namegenerator.GeneralNames,
+			expectedPrefix:      prefix,
+			expectedSuffix:      "",
+		},
+		{
+			description:         "generate default name with suffix",
+			generator:           namegenerator.NewNameGenerator().WithSuffix(suffix),
+			expectedFirstNames:  namegenerator.GeneralNames,
+			expectedSecondNames: namegenerator.GeneralNames,
+			expectedPrefix:      "",
+			expectedSuffix:      suffix,
+		},
+		{
+			description:         "generate default name with prefix and suffix",
+			generator:           namegenerator.NewNameGenerator().WithPrefix(prefix).WithSuffix(suffix),
+			expectedFirstNames:  namegenerator.GeneralNames,
+			expectedSecondNames: namegenerator.GeneralNames,
+			expectedPrefix:      prefix,
+			expectedSuffix:      suffix,
+		},
+		{
+			description:         "generate male name with prefix",
+			generator:           namegenerator.NewNameGenerator().WithGender(namegenerator.Male).WithPrefix(prefix),
+			expectedFirstNames:  namegenerator.MaleNames,
+			expectedSecondNames: namegenerator.FamilyNames,
+			expectedPrefix:      prefix,
+			expectedSuffix:      "",
+		},
+		{
+			description:         "generate male name with suffix",
+			generator:           namegenerator.NewNameGenerator().WithGender(namegenerator.Male).WithSuffix(suffix),
+			expectedFirstNames:  namegenerator.MaleNames,
+			expectedSecondNames: namegenerator.FamilyNames,
+			expectedPrefix:      "",
+			expectedSuffix:      suffix,
+		},
+		{
+			description:         "generate male name with prefix and suffix",
+			generator:           namegenerator.NewNameGenerator().WithGender(namegenerator.Male).WithPrefix(prefix).WithSuffix(suffix),
+			expectedFirstNames:  namegenerator.MaleNames,
+			expectedSecondNames: namegenerator.FamilyNames,
+			expectedPrefix:      prefix,
+			expectedSuffix:      suffix,
+		},
+		{
+			description:         "generate female name with prefix",
+			generator:           namegenerator.NewNameGenerator().WithGender(namegenerator.Female).WithPrefix(prefix),
+			expectedFirstNames:  namegenerator.FemaleNames,
+			expectedSecondNames: namegenerator.FamilyNames,
+			expectedPrefix:      prefix,
+			expectedSuffix:      "",
+		},
+		{
+			description:         "generate female name with suffix",
+			generator:           namegenerator.NewNameGenerator().WithGender(namegenerator.Female).WithSuffix(suffix),
+			expectedFirstNames:  namegenerator.FemaleNames,
+			expectedSecondNames: namegenerator.FamilyNames,
+			expectedPrefix:      "",
+			expectedSuffix:      suffix,
+		},
+		{
+			description:         "generate female name with prefix and suffix",
+			generator:           namegenerator.NewNameGenerator().WithGender(namegenerator.Female).WithPrefix(prefix).WithSuffix(suffix),
+			expectedFirstNames:  namegenerator.FemaleNames,
+			expectedSecondNames: namegenerator.FamilyNames,
+			expectedPrefix:      prefix,
+			expectedSuffix:      suffix,
+		},
+	}
+
+	for _, tc := range testcase {
+		t.Run(tc.description, func(t *testing.T) {
+			name := tc.generator.Generate()
+
+			test(t, name, tc.expectedPrefix, tc.expectedSuffix, tc.expectedFirstNames, tc.expectedSecondNames)
+
+			names := tc.generator.GenerateNames(5)
+
+			if len(names) != 5 {
+				t.Errorf("Expected 5 names, got %d", len(names))
+			}
+
+			for _, name := range names {
+				test(t, name, tc.expectedPrefix, tc.expectedSuffix, tc.expectedFirstNames, tc.expectedSecondNames)
+			}
+		})
 	}
 }
 
-func TestNameGenerator_Generate(t *testing.T) {
-	generator := namegenerator.NewNameGenerator()
-	name := generator.Generate()
-
-	if len(name) == 0 {
+func test(t *testing.T, name, prefix, suffix string, firstNames, secondNames []string) {
+	if name == "" {
 		t.Errorf("Expected a name, got an empty string")
 	}
 
+	if prefix != "" && !strings.HasPrefix(name, prefix) {
+		t.Errorf("Expected a name with a prefix, got %s", name)
+	}
+	name = strings.TrimPrefix(name, prefix)
+
+	if suffix != "" && !strings.HasSuffix(name, suffix) {
+		t.Errorf("Expected a name with a prefix, got %s", name)
+	}
+	name = strings.TrimSuffix(name, suffix)
+
 	if strings.Count(name, "-") == 1 {
 		if !strings.Contains(name, "-") {
-			t.Errorf("Expected a name with a family name separator, got %s", name)
+			t.Errorf("Expected a name does not contain a family name separator, got %s", name)
 		}
 
-		if !contains(namegenerator.GeneralNames, strings.Split(name, "-")[0]) {
-			t.Errorf("Generated name '%s' does not contain a general name", name)
+		if !contains(firstNames, strings.Split(name, "-")[0]) {
+			t.Errorf("Generated name '%s' is not contained in the list of first names", name)
 		}
 
-		if !contains(namegenerator.GeneralNames, strings.Split(name, "-")[1]) {
-			t.Errorf("Generated name '%s' does not contain a general name", name)
-		}
-	}
-	generator = namegenerator.NewNameGenerator().WithGender(namegenerator.Male)
-	name = generator.Generate()
-
-	if strings.Count(name, "-") == 1 {
-		if len(name) == 0 {
-			t.Errorf("Expected a male name, got an empty string")
-		}
-
-		if !strings.Contains(name, "-") {
-			t.Errorf("Expected a male name with a family name separator, got %s", name)
-		}
-
-		if !contains(namegenerator.MaleNames, strings.Split(name, "-")[0]) {
-			t.Errorf("Generated name '%s' does not contain a male name", name)
-		}
-
-		if !contains(namegenerator.FamilyNames, strings.Split(name, "-")[1]) {
-			t.Errorf("Generated name '%s' does not contain a family name", name)
-		}
-	}
-
-	generator = namegenerator.NewNameGenerator().WithGender(namegenerator.Female)
-	name = generator.Generate()
-
-	if strings.Count(name, "-") == 1 {
-		if len(name) == 0 {
-			t.Errorf("Expected a female name, got an empty string")
-		}
-
-		if !strings.Contains(name, "-") {
-			t.Errorf("Expected a female name with a family name separator, got %s", name)
-		}
-
-		if !contains(namegenerator.FemaleNames, strings.Split(name, "-")[0]) {
-			t.Errorf("Generated name '%s' does not contain a female name", name)
-		}
-
-		if !contains(namegenerator.FamilyNames, strings.Split(name, "-")[1]) {
-			t.Errorf("Generated name '%s' does not contain a family name", name)
-		}
-	}
-}
-
-func TestNameGenerator_GenerateNames(t *testing.T) {
-	generator := namegenerator.NewNameGenerator()
-	names := generator.GenerateNames(5)
-
-	if len(names) != 5 {
-		t.Errorf("Expected 5 names, got %d", len(names))
-	}
-
-	for _, name := range names {
-		if len(name) == 0 {
-			t.Errorf("Expected a name, got an empty string")
-		}
-
-		if strings.Count(name, "-") == 1 {
-			if !strings.Contains(name, "-") {
-				t.Errorf("Expected a name with a family name separator, got %s", name)
-			}
-
-			if !contains(namegenerator.GeneralNames, strings.Split(name, "-")[0]) {
-				t.Errorf("Generated name '%s' does not contain a general name", name)
-			}
-
-			if !contains(namegenerator.GeneralNames, strings.Split(name, "-")[1]) {
-				t.Errorf("Generated name '%s' does not contain a general name", name)
-			}
-		}
-	}
-
-	generator = namegenerator.NewNameGenerator().WithGender(namegenerator.Male)
-	names = generator.GenerateNames(5)
-
-	if len(names) != 5 {
-		t.Errorf("Expected 5 male names, got %d", len(names))
-	}
-
-	for _, name := range names {
-		if len(name) == 0 {
-			t.Errorf("Expected a male name, got an empty string")
-		}
-
-		if strings.Count(name, "-") == 1 {
-			if !strings.Contains(name, "-") {
-				t.Errorf("Expected a male name with a family name separator, got %s", name)
-			}
-
-			if !contains(namegenerator.MaleNames, strings.Split(name, "-")[0]) {
-				t.Errorf("Generated name '%s' does not contain a male name", name)
-			}
-
-			if !contains(namegenerator.FamilyNames, strings.Split(name, "-")[1]) {
-				t.Errorf("Generated name '%s' does not contain a family name", name)
-			}
-		}
-	}
-
-	generator = namegenerator.NewNameGenerator().WithGender(namegenerator.Female)
-	names = generator.GenerateNames(5)
-
-	if len(names) != 5 {
-		t.Errorf("Expected 5 female names, got %d", len(names))
-	}
-
-	for _, name := range names {
-		if len(name) == 0 {
-			t.Errorf("Expected a male name, got an empty string")
-		}
-
-		if strings.Count(name, "-") == 1 {
-			if !strings.Contains(name, "-") {
-				t.Errorf("Expected a male name with a family name separator, got %s", name)
-			}
-
-			if !contains(namegenerator.FemaleNames, strings.Split(name, "-")[0]) {
-				t.Errorf("Generated name '%s' does not contain a female name", name)
-			}
-
-			if !contains(namegenerator.FamilyNames, strings.Split(name, "-")[1]) {
-				t.Errorf("Generated name '%s' does not contain a family name", name)
-			}
+		if !contains(secondNames, strings.Split(name, "-")[1]) {
+			t.Errorf("Generated name '%s' is not contained in the list of second names", name)
 		}
 	}
 }
